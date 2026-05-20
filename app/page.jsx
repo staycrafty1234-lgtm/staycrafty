3dd3f4ecb127d7f50c63c09ff7a0cb4e2de5dfde
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-20 11:28:12 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,44 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import ProductGrid from '../components/ProductGrid'
+import About from '../components/About'
+import Reviews from '../components/Reviews'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-hidden">
+    <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-x-hidden">
+
+      {/* Navigation */}
       <Navbar />
+
+      {/* Hero Section */}
       <Hero />
+
+      {/* Products */}
+      <section id="shop">
+        <ProductGrid />
+      </section>
+
+      {/* About Brand */}
+      <section id="about">
+        <About />
+      </section>
+
+      {/* Customer Reviews */}
+      <section id="reviews">
+        <Reviews />
+      </section>
+
+      {/* Contact */}
+      <section id="contact">
+        <Contact />
+      </section>
+
+      {/* Footer */}
+      <Footer />
+
     </main>
   )
 }
