949e3fa1f968acc32680ffd02288bc3ede034ea7
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-21 00:30:19 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from 'react'
+
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ProductGrid from '../components/ProductGrid'
@@ -7,36 +11,28 @@ import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 
 export default function Home() {
+
+  const [cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    setCart([...cart, product])
+  }
+
   return (
     <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-x-hidden">
 
-      {/* Navigation */}
-      <Navbar />
+      <Navbar cartCount={cart.length} />
 
-      {/* Hero Section */}
       <Hero />
 
-      {/* Products */}
-      <section id="shop">
-        <ProductGrid />
-      </section>
+      <ProductGrid addToCart={addToCart} />
 
-      {/* About Brand */}
-      <section id="about">
-        <About />
-      </section>
+      <About />
 
-      {/* Customer Reviews */}
-      <section id="reviews">
-        <Reviews />
-      </section>
+      <Reviews />
 
-      {/* Contact */}
-      <section id="contact">
-        <Contact />
-      </section>
+      <Contact />
 
-      {/* Footer */}
       <Footer />
 
     </main>
