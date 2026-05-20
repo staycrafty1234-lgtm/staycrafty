e0bdd6cfee1491bc949a47e453a292be40885f40
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-20 11:24:58 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-hidden">
       <Navbar />
+      <Hero />
     </main>
   )
 }
