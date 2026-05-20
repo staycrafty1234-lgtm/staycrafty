375b8382e345e53724e6362f845c7a0aeb8242d7
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-20 11:11:12 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,7 @@
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import ProductGrid from '@/components/ProductGrid'
-import About from '@/components/About'
-import Reviews from '@/components/Reviews'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-
 export default function Home() {
   return (
-    <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-hidden">
-      <Navbar />
-      <Hero />
-      <ProductGrid />
-      <About />
-      <Reviews />
-      <Contact />
-      <Footer />
+    <main>
+      <h1>StayCrafty</h1>
     </main>
   )
 }
