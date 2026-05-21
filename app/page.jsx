4edd813f89ff61f616fa288004bc81511be089ea
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-21 20:31:57 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -16,8 +16,52 @@ export default function Home() {
   const [isCartOpen, setIsCartOpen] = useState(false)
 
   const addToCart = (product) => {
-    setCart([...cart, product])
+
+  const existing = cart.find(item => item.id === product.id)
+
+  if (existing) {
+    setCart(
+      cart.map(item =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    )
+  } else {
+    setCart([
+      ...cart,
+      {
+        ...product,
+        quantity: 1
+      }
+    ])
   }
+}
+  const increaseQty = (id) => {
+  setCart(
+    cart.map(item =>
+      item.id === id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    )
+  )
+}
+
+const decreaseQty = (id) => {
+  setCart(
+    cart
+      .map(item =>
+        item.id === id
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+      .filter(item => item.quantity > 0)
+  )
+}
+
+const removeItem = (id) => {
+  setCart(cart.filter(item => item.id !== id))
+}
 
   return (
     <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-x-hidden">
@@ -41,6 +85,9 @@ export default function Home() {
   cart={cart}
   isOpen={isCartOpen}
   setIsOpen={setIsCartOpen}
+  increaseQty={increaseQty}
+  decreaseQty={decreaseQty}
+  removeItem={removeItem}
 />
       <Footer />
 
