992726a5eaaa52038a4a5373cdcf017b03466973
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-23 11:35:20 +0530

    Update ProductGrid.jsx

--- components/ProductGrid.jsx
+++ components/ProductGrid.jsx
@@ -6,31 +6,31 @@ const products = [
   {
     id: 1,
     name: 'Luxury Resin Clock',
-    price: 2,499,
+    price: 2499,
     image: '/products/clock.jpg'
   },
   {
     id: 2,
     name: 'Artistic Serving Board',
-    price: 1,899,
+    price: 1899,
     image: '/products/board.jpg'
   },
   {
     id: 3,
     name: 'Geometric Resin Tray',
-    price: 2,299,
+    price: 2299,
     image: '/products/tray.jpg'
   },
   {
     id: 4,
     name: 'Floral Lazy Susan',
-    price: 1,699,
+    price: 1699,
     image: '/products/lazy.jpg'
   },
   {
     id: 5,
     name: 'Om Decor Piece',
-    price: 1,299,
+    price: 1299,
     image: '/products/om.jpg'
   }
 ]
