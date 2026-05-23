053084b3022530af0bcc729e2e3f087ab7a86358
staycrafty1234-lgtm <staycrafty1234@gmail.com>
2026-05-23 14:21:11 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import Cart from '../components/Cart'
+
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ProductGrid from '../components/ProductGrid'
@@ -9,6 +9,7 @@ import About from '../components/About'
 import Reviews from '../components/Reviews'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Cart from '../components/Cart'
 
 export default function Home() {
 
@@ -17,68 +18,82 @@ export default function Home() {
 
   const addToCart = (product) => {
 
-  const existing = cart.find(item => item.id === product.id)
+    const existing = cart.find(
+      item => item.id === product.id
+    )
 
-  if (existing) {
+    if (existing) {
+      setCart(
+        cart.map(item =>
+          item.id === product.id
+            ? {
+                ...item,
+                quantity: item.quantity + 1
+              }
+            : item
+        )
+      )
+    } else {
+      setCart([
+        ...cart,
+        {
+          ...product,
+          quantity: 1
+        }
+      ])
+    }
+  }
+
+  const increaseQty = (id) => {
     setCart(
       cart.map(item =>
-        item.id === product.id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      )
-    )
-  } else {
-    setCart([
-      ...cart,
-      {
-        ...product,
-        quantity: 1
-      }
-    ])
-  }
-}
-  const increaseQty = (id) => {
-  setCart(
-    cart.map(item =>
-      item.id === id
-        ? { ...item, quantity: item.quantity + 1 }
-        : item
-    )
-  )
-}
-
-const decreaseQty = (id) => {
-  setCart(
-    cart
-      .map(item =>
         item.id === id
-          ? { ...item, quantity: item.quantity - 1 }
+          ? {
+              ...item,
+              quantity: item.quantity + 1
+            }
           : item
       )
-      .filter(item => item.quantity > 0)
-  )
-}
+    )
+  }
 
-const removeItem = (id) => {
-  setCart(cart.filter(item => item.id !== id))
-}
+  const decreaseQty = (id) => {
+    setCart(
+      cart
+        .map(item =>
+          item.id === id
+            ? {
+                ...item,
+                quantity: item.quantity - 1
+              }
+            : item
+        )
+        .filter(item => item.quantity > 0)
+    )
+  }
+
+  const removeItem = (id) => {
+    setCart(
+      cart.filter(item => item.id !== id)
+    )
+  }
 
   return (
     <main className="bg-[#F6F1EA] text-[#2B2B2B] overflow-x-hidden">
 
       <Navbar
-  cartCount={cart.length}
-  setIsCartOpen={setIsCartOpen}
-/>
+        cartCount={cart.length}
+        setIsCartOpen={setIsCartOpen}
+      />
 
       <Hero />
 
       <ProductGrid
-  cart={cart}
-  addToCart={addToCart}
-  increaseQty={increaseQty}
-  decreaseQty={decreaseQty}
-/>
+        cart={cart}
+        addToCart={addToCart}
+        increaseQty={increaseQty}
+        decreaseQty={decreaseQty}
+      />
 
       <About />
 
@@ -87,13 +102,14 @@ const removeItem = (id) => {
       <Contact />
 
       <Cart
-  cart={cart}
-  isOpen={isCartOpen}
-  setIsOpen={setIsCartOpen}
-  increaseQty={increaseQty}
-  decreaseQty={decreaseQty}
-  removeItem={removeItem}
-/>
+        cart={cart}
+        isOpen={isCartOpen}
+        setIsOpen={setIsCartOpen}
+        increaseQty={increaseQty}
+        decreaseQty={decreaseQty}
+        removeItem={removeItem}
+      />
+
       <Footer />
 
     </main>
